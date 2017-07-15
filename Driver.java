3f374d26fffbfcc32d29f1beb0c3f29fde28a00c
peter707 <peter707@vt.edu>
2017-07-15 17:54:03 -0400

    Added logging, ability to replay games

--- Driver.java
+++ Driver.java
@@ -1,19 +1,31 @@
 import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Driver {
 	
 	static boolean DEBUG = false;
-	
-	public static void main(String[] args){
+  static StringBuilder gameLog;	
+	public static void main(String[] args) throws IOException{
 		Scanner input = new Scanner(System.in);
-		Game game = new Game(DEBUG);
+    gameLog = new StringBuilder();
+    String filename = "log" + System.currentTimeMillis();
+    Game game = new Game(DEBUG);
 		while(!game.isOver()){
 			System.out.println(game.boardState.toString());
 			game.printState();
 			boolean moveMade = false;
 			while(!moveMade){
 				String move = input.nextLine();
-				if(game.move(move)){
+        
+        gameLog.append(move + "\n");
+        FileWriter file = new FileWriter(filename);
+				BufferedWriter write = new BufferedWriter(file);
+        write.write(gameLog.toString());
+        write.close();
+        file.close();
+
+        if(game.move(move)){
 					moveMade = true;
 				}
 				else{
